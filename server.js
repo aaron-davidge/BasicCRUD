e7f0eeddd345bc61cea3468cdb4129aee16059bb
Your Name <aaron.davidge@selu.edu>
2021-04-27 17:35:23 -0500

    added nodemon ad dev dependency(run npm run dev)

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ console.log('Listening on http://localhost:3000')
 })
 
 app.get('/', (req, res) => {
+    
     res.sendFile(__dirname + '/index.html')
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
